e4deffca6e245f1d749741f5f35c7230ffe9faee
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-05-19 15:37:15 +0200

    .

--- indexSent.php
+++ indexSent.php
@@ -3,19 +3,16 @@
 define('DB_USER', 'abu');
 define('DB_PASSWORD', 'aburefko159753');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'btt');
+define('DB_NAME', 'btt-software');
 
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die('could not connect' . mysqli_connect_error());
 
 mysqli_set_charset($dbc, "utf8");
 
-
 $firstSign = $_REQUEST['firstSign'];
 $lastSign = $_REQUEST['lastSign'];
 $emailSign = $_REQUEST['emailSign'];
 $passSign = $_REQUEST['passSign'];
-
-
 
 if ($_REQUEST['task'] == "register") {
     $query = "INSERT INTO registacija (name, surname, email, password) VALUES ('$firstSign','$lastSign', '$emailSign', '$passSign')";
@@ -27,5 +24,3 @@ if ($_REQUEST['task'] == "register") {
         echo mysqli_error($dbc);
     }
 }
-
-?>
